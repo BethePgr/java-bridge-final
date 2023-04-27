5452adac84e6aac911476792f61f2cb586dde0e8
SEC <c3392302@naver.com>
2023-04-27 10:39:54 +0900

    feat: 이동하기 위한 U나 D 입력 받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,14 +2,8 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public String readBridgeSize() {
         System.out.println("다리의 길이를 입력해주세요.");
         return Console.readLine();
@@ -19,7 +13,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return Console.readLine();
     }
 
     /**
