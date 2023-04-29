9e69e01f1482a23775b0f283c81bfecfb976cf8e
jongwonpc <c3392302@naver.com>
2023-04-30 01:17:33 +0900

    feat: 게임 재시작 기능 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -10,20 +10,34 @@ public class BridgeController {
 
     InputController inputController = new InputController();
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-
+    private boolean flag = true;
     public void run(){
         String bridgeSize = inputController.readBridgeSize();
         List<String> answerBridge = bridgeMaker.makeBridge(Integer.parseInt(bridgeSize));
         BridgeGame bridgeGame = new BridgeGame(answerBridge);
-        runOneGame(bridgeGame,bridgeSize);
-
+        while(flag) {
+            runOneGame(answerBridge,bridgeGame, bridgeSize);
+        }
     }
 
-    public void runOneGame(BridgeGame bridgeGame,String bridgeSize){
+    public void runOneGame(List<String> answerBridge,BridgeGame bridgeGame,String bridgeSize){
         while(bridgeGame.isCorrect() && bridgeGame.sizeOfBMakingBridge() < Integer.parseInt(bridgeSize)){
             String input = inputController.readMoving();
             bridgeGame.move(input);
             OutputView.printMap(bridgeGame.getMakingBridge());
+        }
+        if(bridgeGame.isCorrect()){
+            OutputView.printResult(bridgeGame.getMakingBridge());
+        }
+        if(!bridgeGame.isCorrect()){
+            String input = inputController.readRestart();
+            if(!bridgeGame.retry(input)){
+                OutputView.printResult(bridgeGame.getMakingBridge());
+                flag = false;
+            }
+            if(bridgeGame.retry(input)){
+                runOneGame(answerBridge,new BridgeGame(answerBridge),bridgeSize);
+            }
         }
     }
 
